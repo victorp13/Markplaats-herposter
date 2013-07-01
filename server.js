eb581f6550277bf04ac7ba26297bb6f1533ec9d9
koen-github <koenvdkruk@gmail.com>
2013-07-01 21:19:00 +0200

    List all current ads

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 var http = require("http");
 fs = require('fs')
 var jsdom = require("jsdom");
+var querystring = require('querystring');
+
 
 var username;
 var password;
-fs.readFile('logins.ini', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-  
-  var dinges = data.split(";");
-  username = dinges[0];
-  password = dinges[1];
-});
+var text = fs.readFileSync('logins.ini', 'utf8') //create new file: logins.ini like this: username;password
+var dinges = text.split(";");
+username = dinges[0];
+password = dinges[1];
 console.log(username);
 console.log(password);
 
@@ -21,38 +17,91 @@ console.log(password);
 
 var https = require("https");
 var test;
-https.get("https://www.marktplaats.nl/account/login.html", function(res) {
-var f_cookies = res.headers["set-cookie"][0].split(';')[0] + ";" +res.headers["set-cookie"][1].split(';')[0];
-var alles;
-console.log(f_cookies);
-  res.on('data', function (chunk) {
-alles += chunk.toString();
+https.get("https://www.marktplaats.nl/account/login.html", function (res) {
+    var f_cookies = res.headers["set-cookie"][0].split(';')[0] + ";" + res.headers["set-cookie"][1].split(';')[0];
+    var alles;
+    console.log(f_cookies);
+    res.on('data', function (chunk) {
+        alles += chunk.toString();
 
+    });
+
+    res.on('end', function () {
+        var token;
+        jsdom.env(
+            alles, ["http://code.jquery.com/jquery.js"], function (errors, window) {
+                token = window.$('[name="nl.marktplaats.xsrf.token"]').val();
+
+                var post_data = querystring.stringify({
+                    'j_username': username,
+                    'j_password': password,
+                    'nl.marktplaats.xsrf.token': token
+                });
+
+                console.log(f_cookies);
+                console.log(post_data);
+
+                var options = {
+                    host: 'www.marktplaats.nl',
+                    path: '/account/securityCheck.html',
+                    port: 443,
+                    method: 'POST',
+                    headers: {
+                        'Cookie': f_cookies,
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Content-Length': post_data.length
+
+                    }
+
+                };
+
+
+                var post_req = https.get(options, function (res) {
+                    console.log(res.headers);
+                    var all_cookies = f_cookies + ";" + res.headers["set-cookie"][1].split(';')[0];
+                    console.log(all_cookies);
+                    var options2 = {
+                        host: 'www.marktplaats.nl',
+                        path: '/mymp/verkopen/sellerAdsBatched.json',
+                        port: 443,
+                        method: 'GET',
+                        headers: {
+                            'Cookie': all_cookies,
+
+
+                        }
+
+                    };
+
+                    https.get(options2, function (res) {
+                        console.log(res.headers);
+
+                        res.on('data', function (chunk) {
+                            console.log(chunk.toString());
+
+                        });
+
+
+                    }).on('error', function (e) {
+                        console.log("Got error: " + e.message);
+                    });
+
+
+
+                }).on('error', function (e) {
+                    console.log("Got error: " + e.message);
+                });
+
+
+                // write parameters to post body  
+                post_req.write(post_data);
+                post_req.end();
+
+            });
+
+    });
+
+
+}).on('error', function (e) {
+    console.log("Got error: " + e.message);
 });
-
-res.on('end', function()
-{
-
-jsdom.env(
-  alles,
-  ["http://code.jquery.com/jquery.js"],
-  function(errors, window) {
-    console.log("contents of a.the-link:", window.$('[name="nl.marktplaats.xsrf.token"]').val());
-  });
-
-  });
-
-
-
-
-
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
-});
-
-
-
-  
-     
-  
-
