753bccd749de6b667343496028f56ca3d13aa02d
koen-github <koenvdkruk@gmail.com>
2013-07-01 20:45:16 +0200

    get token

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require("http");
 fs = require('fs')
+var jsdom = require("jsdom");
+
 var username;
 var password;
 fs.readFile('logins.ini', 'utf8', function (err,data) {
@@ -19,13 +21,36 @@ console.log(password);
 
 var https = require("https");
 var test;
-var nieuw = https.get("https://www.marktplaats.nl/account/login.html", function(res, body, callback) {
-callback(res.headers["set-cookie"][0].split(';')[0] + ";" +res.headers["set-cookie"][1].split(';')[0]);
+https.get("https://www.marktplaats.nl/account/login.html", function(res) {
+var f_cookies = res.headers["set-cookie"][0].split(';')[0] + ";" +res.headers["set-cookie"][1].split(';')[0];
+var alles;
+console.log(f_cookies);
+  res.on('data', function (chunk) {
+alles += chunk.toString();
+
+});
+
+res.on('end', function()
+{
+
+jsdom.env(
+  alles,
+  ["http://code.jquery.com/jquery.js"],
+  function(errors, window) {
+    console.log("contents of a.the-link:", window.$('[name="nl.marktplaats.xsrf.token"]').val());
+  });
+
+  });
+
+
+
+
+
 }).on('error', function(e) {
   console.log("Got error: " + e.message);
 });
 
-console.log(nieuw);
+
 
   
      
