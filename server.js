94834ec8bc5f19f6571964690e683153ad87a306
koen-github <koenvdkruk@gmail.com>
2013-07-02 14:05:33 +0200

    added license file

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
+/*
+    Marktplaats reposter/herposter - automatisch marktplaats advertenties herplaatsen
+    
+    Copyright (C) 2013  koen-github
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Source located here: https://github.com/koen-github/Markplaats-herposter
+
+*/
+
+//Maakt geen gebruik van de marktplaats API maar werkt volledig op scraping.
 var http = require("http");
 fs = require('fs')
 var jsdom = require("jsdom");
@@ -124,7 +147,7 @@ jsdom.env({
     console.log("des", window.$("#vip-ad-description").text().trim());
 
   }
-});
+);
 
 }
 
